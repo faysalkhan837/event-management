cc91944552bb62605670a94ba56036b91e2bb443
faysalkhan837 <faysal.khan837@gmail.com>
2024-05-30 20:16:25 +0600

    just framing the site

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Component/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="">this is home.</h1>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
