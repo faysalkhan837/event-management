b46aa8a09c70c9434735e5faf39f29a4370dd10a
faysa khan <faysal.khan837@gmail.com>
2024-06-02 10:50:08 +0600

    working on home page

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,20 @@
-import Navbar from "../../Component/Navbar/Navbar";
+import Carocel from "../../Component/Carocel/Carocel";
+
 
 
 const Home = () => {
+    
+
     return (
         <div>
-            <Navbar></Navbar>
+            <div className="py-5">
+                <h1 className="text-center text-4xl font-bold text-[#FFCF81]">Seamless Event Planning & Management for <br /> Unforgettable Experiences</h1>
+            </div>
+            <Carocel></Carocel>
+            <div className="py-8">
+                <h1 className="text-center text-4xl font-bold text-[#FFCF81]">Our Services</h1>
+            </div>
+
         </div>
     );
 };
